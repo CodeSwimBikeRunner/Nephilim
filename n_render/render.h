1b410f86a2b483e8e7bad1b2a99bdc31e8d6d628
CodeSwimBikeRun <codeswimbikerun@zetastart.com>
2019-09-18 21:47:58 -0500

    added comments.

--- n_render/render.h
+++ n_render/render.h
@@ -18,6 +18,18 @@ typedef struct Render * RenderPtr;
     */
 typedef void (*Draw)(Point, SDL_Surface);
 
+/*
+    * Initializes the renderer, set the height and width of the app here.
+    */
 RenderPtr render_init(const int height,const int width);
+/*
+    * This will handle the renderloop.
+    ? Possible place for registering of all observables.
+    ? just before this create all threads and observers, when there are no more observers exit?
+    */
 int render_loop(RenderPtr);
+
+/*
+    * This will handle shutting down SDL and closing the window.
+    */
 void render_quit(RenderPtr render);
